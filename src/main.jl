736770b6588eff846307d4224b2e8ad6f0db398b
Phuc Nguyen <npnguyen@syr.edu>
2021-03-19 02:54:52 -0400

    wrote some tests in main.jl and fixed bug in VirtualMachine.jl

--- src/main.jl
+++ src/main.jl
@@ -1,11 +1,33 @@
 include("QViM.jl")
 
 using .QViM
+using LinearAlgebra
 
-quip = @quip [
-    H >> 3;
-    H >> 2;
-    H >> 1;
+quip1 = @quip [
+    X >> 1;
+    X >> 2 | 1;
 ]
 
-println(quip)
+quip2 = @quip [
+    X >> 1;
+    X >> 2 | !1;
+]
+
+quip3 = @quip [
+    X >> 2 | 1;
+]
+
+quip4 = @quip [
+    1 => C[1]     ;
+    X >> 2 | !C[1];
+]
+
+quips = [quip1, quip2, quip3, quip4]
+for quip in quips
+    vm = QVM(quip, 2)
+    execute!(vm)
+    for (i, j) in enumerate([norm(x)^2 for x in vm.wfn])
+        println("$i: $j")
+    end
+    println()
+end
